b605c0455ccc7c8915698d75ae2e24403a2dcad0
Chris Jefferson <caj21@st-andrews.ac.uk>
2007-10-29 13:57:41 +0000

    Add new files
    git-svn-id: svn+ssh://keith.cs.st-and.ac.uk/mnt/raid/svn/repositories/minion-private/branches/gadgets@127 477b24dd-e91e-0410-aa49-980f3df0729b

---
+++ minion/preprocess.h
@@ -0,0 +1,115 @@
+/* Minion Constraint Solver
+   http://minion.sourceforge.net
+   
+   For Licence Information see file LICENSE.txt 
+*/
+
+template<typename Var, typename Vars, typename Prop>
+bool inline check_fail(StateObj* stateObj, Var& var, DomainInt val, Vars& vars, Prop prop, bool checkBounds)
+{
+  Controller::world_push(stateObj);
+  var.propagateAssign(val);
+  prop(stateObj, vars, checkBounds);
+  
+  bool check_failed = getState(stateObj).isFailed();
+  getState(stateObj).setFailed(false);
+  
+  Controller::world_pop(stateObj);
+  
+  return check_failed;
+}
+
+template <typename Var, typename Prop>
+void propagateSAC_internal(StateObj* stateObj, vector<Var>& vararray, Prop prop, bool checkBounds)
+{
+  getQueue(stateObj).propagateQueue();
+  if(getState(stateObj).isFailed())
+	return;
+  
+  bool reduced = true;
+  while(reduced)
+  {
+    reduced = false;
+    for(int i = 0; i < vararray.size(); ++i)
+    {
+      Var& var = vararray[i];
+      if(var.isBound() || checkBounds)
+      {
+        while(check_fail(stateObj, var, var.getMax(), vararray, prop, checkBounds))
+        {
+          reduced = true;
+          var.setMax(var.getMax() - 1);
+          prop(stateObj, vararray, checkBounds);
+          if(getState(stateObj).isFailed())
+            return;
+        }
+        
+        while(check_fail(stateObj, var, var.getMin(), vararray, prop, checkBounds))
+        {
+          reduced = true;
+          var.setMin(var.getMin() + 1);
+          prop(stateObj, vararray, checkBounds);
+          if(getState(stateObj).isFailed())
+            return;
+        }
+      }
+      else
+      {
+        for(DomainInt val = var.getMin(); val <= var.getMax(); ++val)
+        {
+          if(var.inDomain(val) && check_fail(stateObj, var, val, vararray, prop, checkBounds))
+          {
+            reduced = true;
+            var.removeFromDomain(val);
+            prop(stateObj, vararray, checkBounds);
+            if(getState(stateObj).isFailed())
+              return;          
+          }
+        }
+      }
+    }
+  }
+}
+
+struct PropogateGAC
+{
+  template<typename Vars>
+  void operator()(StateObj* stateObj, Vars&, bool)
+  {getQueue(stateObj).propagateQueue();}
+};
+
+struct PropagateSAC
+{
+  template<typename Vars>
+  void operator()(StateObj* stateObj, Vars& vars, bool checkBounds)
+  {propagateSAC_internal(stateObj, vars, PropogateGAC(), checkBounds);}
+};
+
+
+struct PropagateSSAC
+{
+  template<typename Vars>
+  void operator()(StateObj* stateObj, Vars& vars, bool checkBounds)
+  {
+	PropagateSAC sac;
+	propagateSAC_internal(stateObj, vars, sac, checkBounds);
+  }
+};
+
+
+
+
+struct MinionArguments
+{
+  enum PreProcess
+  { None, SAC, SSAC, SACBounds, SSACBounds  };
+  
+  VarOrder order;
+  enum PreProcess preprocess;
+  unsigned random_seed;
+  MinionArguments() : order(ORDER_ORIGINAL), preprocess(None), random_seed((unsigned)time(NULL))
+  { }
+  
+};
+
+void preprocessCSP(StateObj*, MinionArguments::PreProcess, vector<AnyVarRef>&);
