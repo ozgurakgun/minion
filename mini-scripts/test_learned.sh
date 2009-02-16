076dd7e04b679f345c743f227e1038131685eb6e
Neil Moore <ncam@cs.st-andrews.ac.uk>
2009-02-16 15:48:51 +0000

    Add ability to skip the first N learned constraints, so the script can work incrementally.
    git-svn-id: svn+ssh://keith.cs.st-and.ac.uk/mnt/raid/svn/repositories/minion-private/branches/learning@2304 477b24dd-e91e-0410-aa49-980f3df0729b

--- mini-scripts/test_learned.sh
+++ mini-scripts/test_learned.sh
@@ -4,6 +4,9 @@
 #out constraints from the output of the learning solver and check that each one
 #is a correct implied constraint. The instance is given as the 3rd parameter.
 
+#An optional fourth parameter specifies the constraint number to start at,
+#this would be useful to skip those already tested.
+
 #The constraints printed out should be a negative of the learned constraint, in
 #minion input format. The lines should be ADDEDNEG=X where X is the negative of
 #the learned constraint.
@@ -16,6 +19,11 @@ TMPFILE=test.minion
 LEARNOUTPUT=/tmp/rubbish
 LEARNCONS=/tmp/rubbish2
 INSTRUN=/tmp/rubbish3
+if [ $# -eq 4 ]; then #work out where to start
+    FIRST=$4
+else
+    FIRST=1
+fi
 
 echo "Sanity checking the instance:";
 if $REFSOLVER $INSTANCE | grep "Problem solvable?: no" > /dev/null; then
@@ -27,8 +35,6 @@ fi
 echo;
 
 eofline=`grep -n "**EOF**" $INSTANCE | cut -f1 -d:`;
-
-count=0
 
 echo "Running the instance using the learning solver";
 $LEARNSOLVER $INSTANCE > $LEARNOUTPUT;
@@ -44,21 +50,23 @@ echo;
 
 cat $LEARNCONS | while read line; do
     count=$(($count+1));
-    echo "Progress: $count of $numlearned";
-    con=`echo $line | cut -c10-`; #get the con alone
-    head -n $(($eofline-1)) $INSTANCE > $TMPFILE;
-    echo $con >> $TMPFILE;
-    echo "**EOF**" >> $TMPFILE;
-    $REFSOLVER -timelimit $TIMEOUT $TMPFILE > $INSTRUN;
-    if cat $INSTRUN | grep "Problem solvable?: yes" > /dev/null; then
-	echo "WARNING! The negative of the following constraint is not implied";
-	echo;
-	echo $con;
-	echo;
-    elif cat $INSTRUN | grep "Time out." > /dev/null; then
-	echo "Unknown - timed out at $TIMEOUT seconds";
-    else
-	echo "Success!";
+    if [ $count -ge $FIRST ]; then
+	echo "Progress: $count of $numlearned";
+	con=`echo $line | cut -c10-`; #get the con alone
+	head -n $(($eofline-1)) $INSTANCE > $TMPFILE;
+	echo $con >> $TMPFILE;
+	echo "**EOF**" >> $TMPFILE;
+	$REFSOLVER -timelimit $TIMEOUT $TMPFILE > $INSTRUN;
+	if cat $INSTRUN | grep "Problem solvable?: yes" > /dev/null; then
+	    echo "WARNING! The negative of the following constraint is not implied";
+	    echo;
+	    echo $con;
+	    echo;
+	elif cat $INSTRUN | grep "Time out." > /dev/null; then
+	    echo "Unknown - timed out at $TIMEOUT seconds";
+	else
+	    echo "Success!";
+	fi
     fi
 done
     
