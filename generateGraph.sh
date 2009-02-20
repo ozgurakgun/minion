bd9764691a9c40b3b365b4dab5b6e779f24aa552
Chris Jefferson <caj21@st-andrews.ac.uk>
2009-02-20 19:32:21 +0000

    Fix some minor bugs in symmetry detection
    git-svn-id: svn+ssh://keith.cs.st-and.ac.uk/mnt/raid/svn/repositories/minion-private/branches/symmetry_breaking@2326 477b24dd-e91e-0410-aa49-980f3df0729b

--- generateGraph.sh
+++ generateGraph.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 case $3 in
-nauty)
-  GAPCOMMAND="generators";;
-full)
-  GAPCOMMAND="Elements(Group(generators))";;
+GeneratorsBasic|FullGroup|StabChainGroup|StabComplete|OrbitComplete|StabChainME)
+  GAPCOMMAND="$3(generators)";;
+
+StabCompleteN|OrbitCompleteN)
+  GAPCOMMAND="$3($4,generators)";;
 *)
   echo './generateGraph.sh <minion> <instance> nauty/full'
   exit;;
 esac
 $1 -Xgraph $2 > $2.gapin
 cat `dirname $0`/rule1.g >> $2.gapin
+cat `dirname $0`/StabFiles.g >> $2.gapin
 echo 'a := OutputTextFile("'$2.gapout'", false);;' >> $2.gapin
 echo 'H :='$GAPCOMMAND';;' >> $2.gapin
 echo 'gen_constraints(H, varnames, a);' >> $2.gapin
@@ -20,4 +22,4 @@ grep -v "**EOF**" $2 > $2.symbreak
 cat $2.gapout >> $2.symbreak
 echo '**EOF**' >> $2.symbreak
 # cleanup
-rm $2.gapin $2.gapout
+#rm $2.gapin $2.gapout
