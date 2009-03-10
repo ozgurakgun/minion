224ac302afde990f17789f9a07c89d5fad938fb8
kep <kep@477b24dd-e91e-0410-aa49-980f3df0729b>
2009-03-10 15:49:46 +0000

    new symmetry breaking test files
    git-svn-id: svn+ssh://keith.cs.st-and.ac.uk/mnt/raid/svn/repositories/minion-private/branches/symmetry_breaking@2389 477b24dd-e91e-0410-aa49-980f3df0729b

--- generateGraph.sh
+++ generateGraph.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 case $3 in
-GeneratorsBasic|FullGroup|StabChainGroup|StabComplete|OrbitComplete|StabChainME)
+GeneratorsBasic|FullGroup|StabChainGap|StabComplete|OrbitComplete|StabChainME)
   GAPCOMMAND="$3(generators)";;
 
 StabCompleteN|OrbitCompleteN)
   GAPCOMMAND="$3($4,generators)";;
 *)
-  echo './generateGraph.sh <minion> <instance> nauty/full'
+  echo 'Invalid symmetyr braeking method'
   exit;;
 esac
 $1 -Xgraph $2 > $2.gapin
@@ -18,8 +18,8 @@ echo 'gen_constraints(H, varnames, a);' >> $2.gapin
 echo 'quit;' >> $2.gapin
 gap.sh < $2.gapin > /dev/null
 
-grep -v "**EOF**" $2 > $2.symbreak
-cat $2.gapout >> $2.symbreak
-echo '**EOF**' >> $2.symbreak
+grep -v "**EOF**" $2 > $2.$3$4
+cat $2.gapout >> $2.$3$4
+echo '**EOF**' >> $2.$3$4
 # cleanup
-#rm $2.gapin $2.gapout
+rm $2.gapin $2.gapout
