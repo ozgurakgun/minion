1e854cb0af45f753bde237e6f8e562bb2760eeb5
Chris Jefferson <caj21@st-andrews.ac.uk>
2009-03-24 21:15:55 +0000

    Neatness tweaks
    git-svn-id: svn+ssh://keith.cs.st-and.ac.uk/mnt/raid/svn/repositories/minion-private/branches/symmetry_breaking@2445 477b24dd-e91e-0410-aa49-980f3df0729b

--- DIMACS2Minion.sh
+++ DIMACS2Minion.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+if [ "$#" != "2" ]; then
+	echo './DIMACS2Minion <instance> <number of colours>'
+	exit 1
+fi
 
-echo 'MINION 3'
-echo '**VARIABLES**'
-echo 'DISCRETE V[' `grep ^p $1 | awk '{print $3}'` '] {1..'$2'}'
-echo '**CONSTRAINTS**'
-grep ^e $1 | awk '{ print "neq(" , $2 , "," , $3, ")"}'
-echo '**EOF**'
+echo 'MINION 3' > $1.minion
+echo '**VARIABLES**' >> $1.minion
+echo 'DISCRETE V[' `grep ^p $1 | awk '{print $3}'` '] {1..'$2'}' >> $1.minion
+echo '**CONSTRAINTS**' >> $1.minion
+grep ^e $1 | awk '{ print "diseq(V[" , $2 - 1 , "],V[" , $3 - 1, "])"}' >> $1.minion
+echo '**EOF**' >> $1.minion
