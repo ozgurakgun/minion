79173c738a6bd5f6916e9cc18e6b58abe5d9a667
Chris Jefferson <caj21@st-andrews.ac.uk>
2009-04-16 14:16:28 +0000

    Add script to build symmetry results
    git-svn-id: svn+ssh://keith.cs.st-and.ac.uk/mnt/raid/svn/repositories/minion-private/branches/symmetry_breaking@2544 477b24dd-e91e-0410-aa49-980f3df0729b

---
+++ buildAllGraphs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo -n $2
+for i in GeneratorsBasic FullGroup StabChainGap StabComplete OrbitComplete StabChainME; do
+    echo -n ' '`do_time.sh generateGraph.sh $1 $2 $i`
+done
+
+for i in StabCompleteN OrbitCompleteN; do
+    echo -n ' '`do_time.sh  generateGraph.sh $1 $2 $i 1`
+    echo -n ' '`do_time.sh  generateGraph.sh $1 $2 $i 2`
+    echo -n ' '`do_time.sh  generateGraph.sh $1 $2 $i 3`
+done
+
+for i in 2 3 5 10 20 30 40 50; do
+    echo -n ' '`do_time.sh generateGraph.sh $1 $2 RandomElem $i`
+done
