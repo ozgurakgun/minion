f978f58dd5d99ad69e203e93739d2c1732b54149
Chris Jefferson <caj21@st-andrews.ac.uk>
2009-04-22 12:37:24 +0000

    Improve solving shell script
    git-svn-id: svn+ssh://keith.cs.st-and.ac.uk/mnt/raid/svn/repositories/minion-private/branches/symmetry_breaking@2565 477b24dd-e91e-0410-aa49-980f3df0729b

--- buildAllGraphs.sh
+++ buildAllGraphs.sh
@@ -6,11 +6,12 @@ for i in GeneratorsBasic FullGroup StabChainGap StabComplete OrbitComplete StabC
 done
 
 for i in StabCompleteN OrbitCompleteN; do
-    echo -n ' '`do_time.sh  generateGraph.sh $1 $2 $i 1`
-    echo -n ' '`do_time.sh  generateGraph.sh $1 $2 $i 2`
-    echo -n ' '`do_time.sh  generateGraph.sh $1 $2 $i 3`
+    echo -n ' '`do_time.sh generateGraph.sh $1 $2 $i 1`
+    echo -n ' '`do_time.sh generateGraph.sh $1 $2 $i 2`
+    echo -n ' '`do_time.sh generateGraph.sh $1 $2 $i 3`
 done
 
 for i in 2 3 5 10 20 30 40 50; do
     echo -n ' '`do_time.sh generateGraph.sh $1 $2 RandomElem $i`
 done
+echo
