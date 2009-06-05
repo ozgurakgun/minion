f3e57ae27709ba09fb23c1b581ea459a36b21e84
Neil Moore <ncam@cs.st-andrews.ac.uk>
2009-06-05 08:26:05 +0100

    Instances to benchmark diseq, but also scripts to run them.

--- benchmarks/scripts/benchmark-hudson.py
+++ benchmarks/scripts/benchmark-hudson.py
@@ -4,17 +4,19 @@ from threading import Thread
 program = sys.argv[1]
 benchname = sys.argv[2]
 totaltime = 0
+nodes = 0
 
 class BThread (Thread):
   def __init__ (self,benchmarks):
     Thread.__init__(self)
     self.benchmarks = benchmarks
     self.totaltime = 0
+    self.nodes = 0
 
   def run (self):
     for name in self.benchmarks:
       print 'Running ' + name
-      if name.endswith('.minion'):
+      if name.find('.minion'): #also matches .minion.bzip2 etc.
         timer = timeit.Timer('os.system("'+program+' '+name+' > '+name+'.benchmark.out")','import os')
         time = timer.timeit(1)
         if(time < 5.0):
@@ -24,6 +26,10 @@ class BThread (Thread):
         else:
           time = timer.repeat(3,1)
           self.totaltime += min(time)
+        infile = open(name+'.benchmark.out')
+        for line in infile:
+          if(line.startswith("Total Nodes:")):
+            self.nodes += int(line.partition(':')[2])
     
 
 benchmarks = sys.argv[3:]
@@ -40,7 +46,11 @@ current.start()
 for worker in workers:
   worker.join()
   totaltime += worker.totaltime
+  nodes += worker.nodes
 
 FILE = open(benchname + ".benchmark", "w")
 FILE.write('YVALUE=%.3f' % totaltime)
+FILE.close
+FILE = open(benchname + ".nodes.benchmark", "w")
+FILE.write('YVALUE=%d' % nodes)
 FILE.close
