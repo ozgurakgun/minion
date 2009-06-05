854f507af3d32d07d596ee42cba6a671324d1739
Neil Moore <ncam@cs.st-andrews.ac.uk>
2009-06-05 08:26:06 +0100

    Whoops, forgot to uncomment these lines.

--- benchmarks/scripts/runall-hudson-diseqspeed.sh
+++ benchmarks/scripts/runall-hudson-diseqspeed.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-#python benchmarks/scripts/benchmark-hudson.py $1 bibd test_instances/diseq_speed_test/bibdline*.minion
+python benchmarks/scripts/benchmark-hudson.py $1 bibd test_instances/diseq_speed_test/bibdline*.minion
 python benchmarks/scripts/benchmark-hudson.py $1 golomb test_instances/diseq_speed_test/golomb*.minion.bz2 test_instances/diseq_speed_test/ruler*.minion.bz2
-#python benchmarks/scripts/benchmark-hudson.py $1 knight test_instances/diseq_speed_test/knight5.minion
-#python benchmarks/scripts/benchmark-hudson.py $1 ladder test_instances/diseq_speed_test/ladder*.minion
-#python benchmarks/scripts/benchmark-hudson.py $1 langford test_instances/diseq_speed_test/langford*.minion.bz2
-#python benchmarks/scripts/benchmark-hudson.py $1 nqueens test_instances/diseq_speed_test/nqueens*.minion.bz2
-#python benchmarks/scripts/benchmark-hudson.py $1 peg test_instances/diseq_speed_test/peg*.minion
-#python benchmarks/scripts/benchmark-hudson.py $1 quasigroup-element test_instances/diseq_speed_test/qg-element*
-#python benchmarks/scripts/benchmark-hudson.py $1 quasigroup-gacelement test_instances/diseq_speed_test/qg-gacelement*
-#python benchmarks/scripts/benchmark-hudson.py $1 quasigroup-watchelement test_instances/diseq_speed_test/qg-watchelement*
-#python benchmarks/scripts/benchmark-hudson.py $1 sokoban test_instances/diseq_speed_test/sokoban*.minion
-#python benchmarks/scripts/benchmark-hudson.py $1 solitaire test_instances/diseq_speed_test/solitaire*
+python benchmarks/scripts/benchmark-hudson.py $1 knight test_instances/diseq_speed_test/knight5.minion
+python benchmarks/scripts/benchmark-hudson.py $1 ladder test_instances/diseq_speed_test/ladder*.minion
+python benchmarks/scripts/benchmark-hudson.py $1 langford test_instances/diseq_speed_test/langford*.minion.bz2
+python benchmarks/scripts/benchmark-hudson.py $1 nqueens test_instances/diseq_speed_test/nqueens*.minion.bz2
+python benchmarks/scripts/benchmark-hudson.py $1 peg test_instances/diseq_speed_test/peg*.minion
+python benchmarks/scripts/benchmark-hudson.py $1 quasigroup-element test_instances/diseq_speed_test/qg-element*
+python benchmarks/scripts/benchmark-hudson.py $1 quasigroup-gacelement test_instances/diseq_speed_test/qg-gacelement*
+python benchmarks/scripts/benchmark-hudson.py $1 quasigroup-watchelement test_instances/diseq_speed_test/qg-watchelement*
+python benchmarks/scripts/benchmark-hudson.py $1 sokoban test_instances/diseq_speed_test/sokoban*.minion
+python benchmarks/scripts/benchmark-hudson.py $1 solitaire test_instances/diseq_speed_test/solitaire*
