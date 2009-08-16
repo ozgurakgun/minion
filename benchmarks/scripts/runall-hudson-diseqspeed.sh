acc334204ad94cf3c5456e274ce99adb005378d3
Neil Moore <ncam@cs.st-andrews.ac.uk>
2009-08-16 15:31:37 +0100

    Remove peg solitaire from run script.

--- benchmarks/scripts/runall-hudson-diseqspeed.sh
+++ benchmarks/scripts/runall-hudson-diseqspeed.sh
@@ -5,7 +5,6 @@ python benchmarks/scripts/benchmark-hudson.py $1 knight test_instances/diseq_spe
 python benchmarks/scripts/benchmark-hudson.py $1 ladder test_instances/diseq_speed_test/ladder*.minion
 python benchmarks/scripts/benchmark-hudson.py $1 langford test_instances/diseq_speed_test/langford*.minion.bz2
 python benchmarks/scripts/benchmark-hudson.py $1 nqueens test_instances/diseq_speed_test/nqueens*.minion.bz2
-python benchmarks/scripts/benchmark-hudson.py $1 peg test_instances/diseq_speed_test/peg*.minion
 python benchmarks/scripts/benchmark-hudson.py $1 quasigroup-element test_instances/diseq_speed_test/qg-element*
 python benchmarks/scripts/benchmark-hudson.py $1 quasigroup-gacelement test_instances/diseq_speed_test/qg-gacelement*
 python benchmarks/scripts/benchmark-hudson.py $1 quasigroup-watchelement test_instances/diseq_speed_test/qg-watchelement*
