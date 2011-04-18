0ccdd972cd40fc958fc7b8c5cba4d183a57af126
Chris Jefferson <caj21@st-andrews.ac.uk>
2011-04-18 20:10:29 +0100

    Further tweaks

--- do_test.sh
+++ do_test.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 if [ "$#" = "0" ]; then
-	 echo './do_test.sh <minion> <instance> <method> <method parameter (optional)>'
+	 echo './do_test.sh <filename>'
 	 exit 1
 fi
 
-./generateGraph.sh $*
-filename=$2.$3$4
-echo $filename
-singlesol=`minion-svn $filename           | grep "^Total Nodes\|^Solutions Found\|^Total Time" | awk '{print $3}'`
-allsol=`minion-svn $filename -findallsols | grep "^Total Nodes\|^Solutions Found\|^Total Time" | awk '{print $3}'`
+echo -n $1 ' & ' > $1.ans
+singlesol=`minion $1          -noprintsols -cpulimit 300 | grep "^Total Nodes\|^Solutions Found\|^Total Time" | awk '{print $3}'`
+allsol=`minion $1 -findallsols -noprintsols -cpulimit 300 | grep "^Total Nodes\|^Solutions Found\|^Total Time" | awk '{print $3}'`
 
-echo $singlesol $allsol | awk '{print $1," & ",$2," & ",$3," & ",$4," & ",$5," & ",$6}'
+echo $singlesol $allsol | awk '{print $1," & ",$2," & ",$3," & ",$4," & ",$5," & ",$6}' >> $1.ans
